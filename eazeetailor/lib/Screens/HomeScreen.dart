6809b61b8d1c0788d2b952330f984f21eb7f8c81
Nurhafizah Shukri <nfizaaaah@gmail.com>
2020-04-08 14:39:03 +0800

    App Bar

--- eazeetailor/lib/Screens/HomeScreen.dart
+++ eazeetailor/lib/Screens/HomeScreen.dart
@@ -1,13 +1,62 @@
-
+import 'package:eazeetailor/Constant/Constant.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+class HomeScreen extends StatefulWidget {
+  var userName;
 
-class HomeScreen extends StatelessWidget {
+  HomeScreen(this.userName);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  void _doNothing() {}
+  String userName;
+  
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: buildAppBar(context),
+      ),
     );
   }
+
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+        leading: Builder(
+          builder: (context) => IconButton(
+                icon: new Icon(Icons.notifications_active),
+                onPressed: () => Scaffold.of(context).openDrawer(),
+              ),
+      ),
+        automaticallyImplyLeading: true,
+        backgroundColor: Color(0xFF0097A7),
+        centerTitle: true,
+        title: new Text("EAZEE TAILOR",
+        style: GoogleFonts.pacifico(
+        //textStyle: TextStyle(color: Colors.black, letterSpacing: .5),
+        ),),
+        actions: <Widget>[
+          IconButton(
+            icon: new Icon(Icons.search),
+            onPressed: _doNothing,
+          ),
+          IconButton(
+              icon: new Icon(Icons.power_settings_new),
+              onPressed: () {
+                Navigator.of(context).pushReplacementNamed(LOGIN_SCREEN);
+              }),
+        ],
+      );
+  }
 }
+
+
+
