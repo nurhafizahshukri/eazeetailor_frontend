6344202ff2c4b6e4cba09a15dbe6e8dd6ec7fccb
Husna <husna.azmi98@gmail.com>
2020-04-11 02:23:57 +0800

    Create DateTime

--- eazeetailor/lib/Screens/AppointmentForm.dart
+++ eazeetailor/lib/Screens/AppointmentForm.dart
@@ -1,6 +1,8 @@
 import 'package:eazeetailor/Constant/Constant.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 
 class AppointmentScreen extends StatefulWidget {
   
@@ -11,6 +13,8 @@ class AppointmentScreen extends StatefulWidget {
 class _AppointmentScreenState extends State<AppointmentScreen> {
   int photoIndex=0;
 
+  final format = DateFormat("yyyy-MM-dd");
+  final format1 = DateFormat("HH:mm");
   void _doNothing() {}
 
   @override
@@ -34,6 +38,59 @@ class _AppointmentScreenState extends State<AppointmentScreen> {
           ],
         
         ),
+        body: Center( 
+        //child: Column(
+          child: ListView(
+            //padding: EdgeInsets.all(50),
+            children: <Widget> [
+              Padding(padding: EdgeInsets.only(top: 20.0),),
+              Text('APPOINTMENT',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  ),
+                ),
+              Padding(padding: EdgeInsets.only(top: 30.0),),
+              Text('Appointment Date',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 15),),
+              Padding(padding: EdgeInsets.only(left: 30, right: 30, bottom: 15, top: 15),
+                child: DateTimeField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(3)),
+                    labelText: 'Date Format: (${format.pattern})'
+                  ),
+                  format: format,
+                  onShowPicker: (context, currentValue) {
+                    return showDatePicker(
+                      context: context,
+                      firstDate: DateTime(1900),
+                      initialDate: currentValue ?? DateTime.now(),
+                      lastDate: DateTime(2100));
+                  },
+                ),
+              ),
+
+              Text('Appointment Time',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 15),),
+              Padding(padding: EdgeInsets.only(left: 30, right: 30, bottom: 15, top: 15),
+                child:DateTimeField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(3)),
+                    labelText: 'Time Format: (${format1.pattern})'
+                  ),
+                  format: format1,
+                  onShowPicker: (context, currentValue) async {
+                    final time = await showTimePicker(
+                      context: context,
+                      initialTime: TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+                    );
+                    return DateTimeField.convert(time);
+                  },
+                ), ),
+            ]),     ),  
         );
         
   }
