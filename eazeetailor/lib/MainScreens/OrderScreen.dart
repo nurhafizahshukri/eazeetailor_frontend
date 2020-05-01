5d32d912439f5e7bc49f053d4bdfd7c101337a53
dayah <nurhidayah9707@gmail.com>
2020-05-01 20:54:48 +0800

    orderscreen p3

--- eazeetailor/lib/MainScreens/OrderScreen.dart
+++ eazeetailor/lib/MainScreens/OrderScreen.dart
@@ -1,21 +1,36 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../models/class.dart';
+
+
 class OrderScreen extends StatefulWidget {
+  final List<Order> orderDetails;
+
+  OrderScreen(this.orderDetails);
+
   @override
   _OrderScreenState createState() => _OrderScreenState();
 }
 
 class _OrderScreenState extends State<OrderScreen> {
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.separated(
-          itemBuilder: (context, int index) => ListTile(
+      body: Container(
+        child: ListView.separated(
+          itemBuilder: (context, index) 
+          => ListTile(
               leading: Icon(Icons.note),
-              title: Text('Order 1'),
-              subtitle: Text('Design: \nSize: \nFabric: ')),
+              title: Text('Order ${[index]}'),
+              subtitle: Text('Design: \nSize: \nFabric: ')
+              ),
           separatorBuilder: (context, index) => Divider(color: Colors.black),
-          itemCount: 1),
+          itemCount: widget.orderDetails.length),
+      ),
+
       floatingActionButton: Row(
         children: <Widget>[
           FloatingActionButton.extended(
