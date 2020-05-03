0887c2b142375b018d1a4e91b5fb21f191fb138c
Husna <husna.azmi98@gmail.com>
2020-05-03 23:04:48 +0800

    Appointment mock data

---
+++ eazeetailor/lib/models/ApptData.dart
@@ -0,0 +1,9 @@
+import 'package:eazeetailor/models/class.dart';
+
+import 'class.dart';
+
+List<Appointment> mockBooking = [
+  Appointment('22/12/2020','11:00'),
+  Appointment('13/5/2020', '4:30'),
+  Appointment('07/08/2020','2:15'),
+];
