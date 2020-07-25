4665035d4a216848d65cb622b47249102b4d0f43
Dayah97 <nurhidayah9707@gmail.com>
2020-07-25 16:48:03 +0800

    Update order form screen

--- eazeetailor/lib/SubScreens/OrderFormScreen.dart
+++ eazeetailor/lib/SubScreens/OrderFormScreen.dart
@@ -1,10 +1,10 @@
 import 'package:eazeetailor/Constant/Constant.dart';
 import 'package:eazeetailor/MainScreens/HomeScreen.dart';
 import 'package:eazeetailor/models/class.dart';
+import 'package:eazeetailor/services/data_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:eazeetailor/SubScreens/AppointmentForm.dart';
 
 class OrderForm extends StatefulWidget {
 
@@ -17,7 +17,16 @@ class OrderForm extends StatefulWidget {
 }
 
 class _OrderFormState extends State<OrderForm> {
-  int selectedRadio;
+  List<Members> _member;
+  List<Order> _order;
+  String _fabric, _size;
+  GlobalKey<FormState> _formKey;
+
+  void initState() {
+    _formKey = GlobalKey<FormState>();
+    super.initState();
+  }
+
   String _valFriends;
   List _myFriends = [
     "Mom",
@@ -29,6 +38,18 @@ class _OrderFormState extends State<OrderForm> {
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<List<Members>>(
+      future: dataService.getMembersList(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          _member = snapshot.data;
+          return _buildScreen();
+        }
+        return _buildFetchingDataScreen();
+      });
+  }
+
+  Scaffold _buildScreen() {
     return Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading: true,
@@ -49,122 +70,133 @@ class _OrderFormState extends State<OrderForm> {
         ),
         body: Center( 
         //child: Column(
-          child: ListView(
-            //padding: EdgeInsets.all(50),
-            children: <Widget> [
-              Padding(padding: EdgeInsets.only(top: 20.0),),
-              Text(widget.outfits.title,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  ),
-                ),
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              //padding: EdgeInsets.all(50),
+              children: <Widget> [
                 Padding(padding: EdgeInsets.only(top: 20.0),),
-                CarouselSlider(
-                  height: 300.0,
-                  enlargeCenterPage: true,
-                  enableInfiniteScroll: false,
-                  initialPage: 1,
-                  items: widget.outfits.imgurl.map((item) => Container(
-                  child: Center(
-                    child: Image.asset(item, fit: BoxFit.cover, width: 1000)
-                  ),
-                  )).toList(),
-                ),
-                Container(
-                  height: 60,
-                  margin: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2,
-                      ),
-                      color: Colors.white,
-                  ),
-                  child: Center(
-                    child: Text(widget.outfits.desc,
-                      textAlign: TextAlign.center,
-                    )
-                  ),
-                ),
-              Text('Size :', textAlign: TextAlign.center,),
-              Center(
-                child: DropdownButton(
-                hint: Text("Select Your Size"),
-                value: _valFriends,
-                items: _myFriends.map((value) {
-                  return DropdownMenuItem(
-                    child: Text(value,
-                    textAlign: TextAlign.center,),
-                    value: value,
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _valFriends = value;
-                  });
-                },
-            ),
-              ),
-              Padding(padding: EdgeInsets.only(left: 30, right: 30, bottom: 20),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Fabric type',
-                      hintText: 'Fabric type',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(3)
-                      )
-                  ),
-                ),
-              ),
-              Padding(padding: EdgeInsets.only(left: 30, right: 30, bottom: 20),
-                  child: TextFormField(
-                    maxLines: 4,
-                    decoration: InputDecoration(
-                      labelText: 'Remarks',
-                      hintText: 'Remarks',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(3)
-                      )
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                FlatButton(
-                  child: Text('Cancel'),
-                  onPressed: () {
-                    Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => HomeScreen(),
-                        ),
-                      );
-                  },
-                  ),
-                RaisedButton(
-                  color: Colors.cyan,
-                  onPressed: () {
-                      Navigator.popUntil(context, (route) => route.isFirst);
-                  },
-                  child: const Text(
-                    'Submit',
-                    style: TextStyle(fontSize: 20)
+                Text(widget.outfits.title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
                     ),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 20.0),),
+                  CarouselSlider(
+                    height: 300.0,
+                    enlargeCenterPage: true,
+                    enableInfiniteScroll: false,
+                    initialPage: 1,
+                    items: widget.outfits.imgurl.map((item) => Container(
+                    child: Center(
+                      child: Image.asset(item, fit: BoxFit.cover, width: 1000)
+                    ),
+                    )).toList(),
+                  ),
+                  Container(
+                    height: 60,
+                    margin: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                        color: Colors.white,
+                    ),
+                    child: Center(
+                      child: Text(widget.outfits.desc,
+                        textAlign: TextAlign.center,
+                      )
+                    ),
+                  ),
+                Text('Size :', textAlign: TextAlign.center,),
+                Center(
+                  child: DropdownButton(
+                  hint: Text("Select Your Size"),
+                  value: _size,
+                  items: _member
+                  .map(
+                    (value) => DropdownMenuItem(
+                      value: value,
+                      child: Text(value.name),
+                    ),
+                  ).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _size = value;
+                    });
+                  },
+              ),
                 ),
-              ],) 
-               
-              //)
-              
-            ]
-          
+                Padding(padding: EdgeInsets.only(left: 30, right: 30, bottom: 20),
+                    child: TextFormField(
+                      onSaved: (currentValue) => _fabric = currentValue,
+                      decoration: InputDecoration(
+                        labelText: 'Fabric type',
+                        hintText: 'Fabric type',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(3)
+                        )
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                  FlatButton(
+                    child: Text('Cancel'),
+                    onPressed: () {
+                      Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => HomeScreen(),
+                          ),
+                        );
+                    },
+                    ),
+                  RaisedButton(
+                    color: Colors.cyan,
+                    onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                      _formKey.currentState.save();
+                        final newOrder = await dataService.createOrder(
+                      orders: Order(design: widget.outfits.title , size: _size, fabric: _fabric),);
+                      setState(() => _order.add(newOrder));
+                        Navigator.popUntil(context, (route) => route.isFirst);
+                    }},
+                    child: const Text(
+                      'Submit',
+                      style: TextStyle(fontSize: 20)
+                      ),
+                  ),
+                ],) 
+                 
+                //)
+                
+              ]
+            
         ),
+          ),
           //),
         ),
         );
         
+  }
+  Scaffold _buildFetchingDataScreen() {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircularProgressIndicator(),
+            SizedBox(height: 50),
+            Text('Fetching todo... Please wait'),
+          ],
+        ),
+      ),
+    );
   }
 }
 
--- eazeetailor/lib/models/OrderData.dart
+++
@@ -1,9 +0,0 @@
-import 'package:eazeetailor/models/class.dart';
-
-import 'class.dart';
-
-List<Order> mockOrder = [
-  Order('Jubah', 'Me', 'chiffon'),
-  Order('Kurung Kedah', 'Mom', 'corduroy'),
-  Order('Kebarung', 'Me', 'cotton'),
-];
